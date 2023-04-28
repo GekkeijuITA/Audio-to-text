2a91c73dc6288461b1a11c8bd20c5ef81ea5aaea
Gekkeiju <gekkeijuita@gmail.com>
2023-04-28 11:17:50 +0200

    Initialize GUI

--- main.py
+++ main.py
@@ -2,21 +2,32 @@ import speech_recognition as sr
 import subprocess
 import os
 import logging
+import PySimpleGUI as sg
+import platform
 
 logging.basicConfig(level=logging.WARNING)
 audio_format = ['.mp3', '.wav', '.m4a']
 folder = '.convertedFile'
+language = 'it-IT'
+
+current_platform = platform.system()
+
+if current_platform == 'Windows':
+    ffmpeg_path = os.path.join(os.getcwd(), 'ffmpeg', 'windows', 'ffmpeg.exe')
+elif current_platform == 'Darwin':
+    ffmpeg_path = os.path.join(os.getcwd(), 'ffmpeg', 'macos', 'ffmpeg')
+else:
+    ffmpeg_path = os.path.join(os.getcwd(), 'ffmpeg', 'linux', 'ffmpeg')
 
 
 def get_file_name(audio_file):
     file_name = audio_file.split('.')[0]
     return file_name
 
-
 def convert_to_wav(audio_file, title):
     if os.path.exists(folder + '/' + title + '.wav'):
         os.remove(folder + '/' + title + '.wav')
-    subprocess.call(['ffmpeg', '-i', audio_file, '-vn', '-acodec',
+    subprocess.call([ffmpeg_path, '-i', audio_file, '-vn', '-acodec',
                     'pcm_s16le', '-ar', '44100', '-ac', '2', folder + '/' + title + '.wav'])
 
 
@@ -43,19 +54,26 @@ def delete_files_in_folder(folder):
 def audio_to_text(audio_file):
     title = get_file_name(audio_file)
     convert_to_wav(audio_file, title)
-    with open(title + ".txt", 'w', encoding='utf-8') as file:
+    with open("transcription/" + title + ".txt", 'w', encoding='utf-8') as file:
         file.write(transcribe_audio(folder + '/' + title + '.wav', language))
     delete_files_in_folder(folder)
 
+# Creazione della finestra
+layout = [[sg.Text('Seleziona uno o più file')],
+          [sg.Input(key='_FILESBROWSE_'), sg.FilesBrowse()],
+          [sg.OK(), sg.Cancel()]]
+window = sg.Window('Seleziona uno o più file', layout)
 
-language = 'it-IT'
-# count files in directory
-count = len([name for name in os.listdir('.') if os.path.isfile(
-    name) and name.endswith(tuple(audio_format))])
-if count > 0:
-    print('Transcribing audio files...')
-    for audio_file in os.listdir("."):
-        if audio_file.endswith(tuple(audio_format)):
+# Loop degli eventi
+while True:
+    event, values = window.read()
+    if event in (None, 'Cancel'):
+        break
+    elif event == 'OK':
+        files_selected = values['_FILESBROWSE_'].split(';')
+        for audio_file in files_selected:
             audio_to_text(audio_file)
-else:
-    print('No audio files found in the directory')
+        break
+
+# Chiusura della finestra
+window.close()
