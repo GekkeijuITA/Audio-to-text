500253216474f7d80e6a83e5e477418c2fcbcf0d
Gekkeiju <gekkeijuita@gmail.com>
2023-04-28 08:55:32 +0200

    Branch aggiornata

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import speech_recognition as sr
 import subprocess
 import os
+import logging
 
-def convert_to_wav(audio_file):
-    if os.path.exists('audio.wav'):
-        os.remove('audio.wav')
+logging.basicConfig(level=logging.WARNING)
+audio_format = ['.mp3', '.wav', '.m4a']
+folder = '.convertedFile'
+
+
+def get_file_name(audio_file):
+    file_name = audio_file.split('.')[0]
+    return file_name
+
+
+def convert_to_wav(audio_file, title):
+    if os.path.exists(folder + '/' + title + '.wav'):
+        os.remove(folder + '/' + title + '.wav')
     subprocess.call(['ffmpeg', '-i', audio_file, '-vn', '-acodec',
-                    'pcm_s16le', '-ar', '44100', '-ac', '2', 'audio.wav'])
+                    'pcm_s16le', '-ar', '44100', '-ac', '2', folder + '/' + title + '.wav'])
 
 
 def transcribe_audio(audio_file, language):
@@ -15,18 +26,36 @@ def transcribe_audio(audio_file, language):
         audio = r.record(source)
     try:
         text = r.recognize_google(audio, language=language)
+        print('Transcription of ' + audio_file + ' complete')
         return text
     except sr.UnknownValueError:
-        print('Sorry, I could not understand audio')
+        print('Sorry, I could not understand the audio ' + audio_file)
+        return ''
     except sr.RequestError as e:
         print('Sorry, my speech service is down {0}'.format(e))
+        return ''
+
+def delete_files_in_folder(folder):
+    for file in os.listdir(folder):
+        if file.endswith(tuple(audio_format)):
+            os.remove(os.path.join(folder, file))
+
+def audio_to_text(audio_file):
+    title = get_file_name(audio_file)
+    convert_to_wav(audio_file, title)
+    with open(title + ".txt", 'w', encoding='utf-8') as file:
+        file.write(transcribe_audio(folder + '/' + title + '.wav', language))
+    delete_files_in_folder(folder)
 
 
 language = 'it-IT'
-
-convert_to_wav('audio.mp3')
-
-audio_file = 'audio.wav'
-
-with open('trascrizione.txt' , 'w' , encoding='utf-8') as file:
-    file.write(transcribe_audio(audio_file, language))
+# count files in directory
+count = len([name for name in os.listdir('.') if os.path.isfile(
+    name) and name.endswith(tuple(audio_format))])
+if count > 0:
+    print('Transcribing audio files...')
+    for audio_file in os.listdir("."):
+        if audio_file.endswith(tuple(audio_format)):
+            audio_to_text(audio_file)
+else:
+    print('No audio files found in the directory')
